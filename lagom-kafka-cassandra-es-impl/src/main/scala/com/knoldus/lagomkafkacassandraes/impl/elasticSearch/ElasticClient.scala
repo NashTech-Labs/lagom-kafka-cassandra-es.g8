97e00fe8b80f0c8e48787cfdaed794a80615d261
Muskan Gupta <muskan.gupta@knoldus.com>
2020-04-09 17:14:22 +0530

    Modfied files

--- lagom-kafka-cassandra-es-impl/src/main/scala/com/knoldus/lagomkafkacassandraes/impl/elasticSearch/ElasticClient.scala
+++ lagom-kafka-cassandra-es-impl/src/main/scala/com/knoldus/lagomkafkacassandraes/impl/elasticSearch/ElasticClient.scala
@@ -1,14 +1,20 @@
 package com.knoldus.lagomkafkacassandraes.impl.elasticSearch
+import akka.actor.ActorSystem
+import akka.kafka.{ConsumerSettings, Subscriptions}
+import akka.kafka.scaladsl.Consumer
+import akka.stream.ActorMaterializer
 import akka.{Done, NotUsed}
 import akka.stream.alpakka.elasticsearch.scaladsl.{ElasticsearchFlow, ElasticsearchSink}
 import akka.stream.alpakka.elasticsearch.{WriteMessage, WriteResult}
-import akka.stream.scaladsl.{Flow, Sink}
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import com.knoldus.lagomkafkacassandraes.api.Product
 import org.apache.http.HttpHost
-import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
+import org.apache.kafka.common.serialization.{ByteArrayDeserializer, StringDeserializer}
 import org.elasticsearch.client.RestClient
 import play.api.libs.json.Json
-import spray.json.{JsNumber, JsObject, JsString, JsonWriter}
+import spray.json.DefaultJsonProtocol.jsonFormat2
+import spray.json.{JsNumber, JsObject, JsString, JsonFormat, JsonWriter}
 
 import scala.concurrent.Future
 
@@ -25,7 +31,7 @@ object ElasticClient {
      "name" -> JsString(product.name),
   "quantity"-> JsNumber(product.quantity))
  }
-////val flow: Flow[WriteMessage[Nothing, NotUsed], WriteResult[Nothing, NotUsed], NotUsed] =ElasticsearchFlow.create("productIndex","docs",ElasticsearchWriteSettings.Default,client,objectMapper)
+//////val flow: Flow[WriteMessage[Nothing, NotUsed], WriteResult[Nothing, NotUsed], NotUsed] =ElasticsearchFlow.create("productIndex","docs",ElasticsearchWriteSettings.Default,client,objectMapper)
 //  val interFlow: Flow[ConsumerRecord[Array[Byte], String], WriteMessage[Product, NotUsed], NotUsed] = Flow[ConsumerRecord[Array[Byte], String]].map { message =>
 //
 //    // Parsing the record as Company Object
@@ -37,14 +43,55 @@ object ElasticClient {
 //
 //    WriteMessage.createIndexMessage(id,product)
 //  }
-//  //val sink = Sink.fromGraph(GraphDSL.create(esSink))
-// val flow: Flow[Product, Done.type, NotUsed] = Flow[Product]
-//   .map(product => WriteMessage.createIndexMessage(product.id,product)).map(_ => Done)
-////val flow: Flow[Any, Nothing, Nothing] = Flow.fromGraph(GraphDSL.create(interFlow))
+////  //val sink = Sink.fromGraph(GraphDSL.create(esSink))
+//// val flow: Flow[Product, Done.type, NotUsed] = Flow[Product]
+////   .map(product => WriteMessage.createIndexMessage(product.id,product)).map(_ => Done)
+//////val flow: Flow[Any, Nothing, Nothing] = Flow.fromGraph(GraphDSL.create(interFlow))
 //  val esSink: Sink[WriteMessage[Product, NotUsed], Future[Done]] = ElasticsearchSink
 //    .create[Product]("productIndex","products")(client,jsonWriter)
 val esSink: Flow[WriteMessage[Product, NotUsed], WriteResult[Product, NotUsed], NotUsed] =
 ElasticsearchFlow.create[Product]("productIndex","products")(client,jsonWriter)
 
-
+//  implicit val system: ActorSystem = ActorSystem.create()
+//
+//  implicit val mat: ActorMaterializer = ActorMaterializer()
+//  implicit val _ = Json.format[Product]
+//
+//  implicit val client: RestClient = RestClient.builder(new HttpHost("localhost", 9200)).build()
+//
+//
+//  implicit val format: JsonFormat[Company] = jsonFormat2(Company)
+//
+//
+//  val intermediateFlow = Flow[ConsumerRecord[Array[Byte], String]].map { kafkaMessage =>
+//
+//    // Parsing the record as Company Object
+//    val company = Json.parse(kafkaMessage.value()).as[Company]
+//    val loc = company.location
+//
+//
+//    // Transform message so that we can write to elastic
+//
+//    WriteMessage.createIndexMessage(loc, company)
+//  }
+//
+//
+//  val consumerSettings = ConsumerSettings(system, new ByteArrayDeserializer, new StringDeserializer)
+//    .withBootstrapServers("localhost:9092")
+//    .withGroupId("akka-stream-kafka-test")
+//    .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+//
+//  val kafkaSource: Source[ConsumerRecord[Array[Byte], String], Consumer.Control] =
+//    Consumer.plainSource(consumerSettings, Subscriptions.topics("topic221"))
+//
+//
+//  val esSink = ElasticsearchSink.create[Company](
+//    indexName = "sink1",
+//    typeName = "company"
+//  )
+//
+//
+//  kafkaSource
+//    .via(intermediateFlow)
+//    .runWith(esSink)
 }
--- lagom-kafka-cassandra-es-impl/src/main/scala/com/knoldus/lagomkafkacassandraes/impl/kafka/TopicSubscriber.scala
+++ lagom-kafka-cassandra-es-impl/src/main/scala/com/knoldus/lagomkafkacassandraes/impl/kafka/TopicSubscriber.scala
@@ -1,44 +1,57 @@
 package com.knoldus.lagomkafkacassandraes.impl.kafka
 
+import akka.stream.ActorMaterializer
 import akka.{Done, NotUsed}
 import akka.stream.alpakka.elasticsearch.WriteMessage
 import akka.stream.scaladsl.Flow
 import com.knoldus.lagomkafkacassandraes.api.{Product, ProductKafkaApi}
 import com.knoldus.lagomkafkacassandraes.impl.elasticSearch.ElasticClient
+import com.knoldus.lagomkafkacassandraes.impl.eventsourcing.ProductEntity
+import com.knoldus.lagomkafkacassandraes.impl.eventsourcing.commands.AddProduct
 import com.lightbend.lagom.scaladsl.persistence.PersistentEntityRegistry
 
 import scala.concurrent.ExecutionContext
 
 class ProductServiceFlow(registry: PersistentEntityRegistry)(implicit ec: ExecutionContext) {
-
-//  val productDetailsFlow: Flow[Product, Product, NotUsed] = Flow[Product].mapAsync(8) {
-//    product =>
-//      registry.refFor[ProductEntity](product.id).ask {
-//       AddProduct(product)
-//      }.map(_ => product)
-//  }
-  val productTopicDetailsFlow: Flow[Product, Done.type, NotUsed] = Flow[Product].map { product =>
-    println(s"\n\n$product\n\n")
-  product
-  }.map(product => Product(product.id, product.name, product.quantity))
-    .map(product => WriteMessage.createIndexMessage(product.id, product)).via(ElasticClient.esSink).map { messageResults =>
-    if (!messageResults.success) {
-      //      log.error(s"Error writing $docType objects to $esIndex: ${result.error.getOrElse("")}")
-      throw new Exception(s"Failed to write to ES")
-    }
-    Done
+ // implicit val mat = ActorMaterializer()
+  val productDetailsFlow: Flow[Product, Product, NotUsed] = Flow[Product].mapAsync(8) {
+    product =>
+      registry.refFor[ProductEntity](product.id).ask {
+       AddProduct(product)
+      }.map(_ => product)
   }
+//  val productToEsFlow: Flow[Product, Done.type, NotUsed] = Flow[Product].via(productDetailsFlow).map { product =>
+//    println(s"\n\n$product\n\n")
+//  product
+//  }.map(product => Product(product.id, product.name, product.quantity).
+//
+    //.map { messageResults =>
+//    if (!messageResults.success) {
+//      //      log.error(s"Error writing $docType objects to $esIndex: ${result.error.getOrElse("")}")
+//      throw new Exception(s"Failed to write to ES")
+//    }
+//    Done
+//  }
 //  val productTopicDetailsFlow: Flow[Product, Done.type, NotUsed] = Flow[Product]
 //    .via(productDetailsFlow).map(_ => productKafkaOuterApi.productDetailsTopic).map(_=> Done)
 ////val productToEsFlow : Sink[Product, NotUsed]= Flow[Product]
 ////  .via(productDetailsFlow)
 ////  .via(ElasticClient.flow)
 ////  .to(sink = ElasticClient.esSink )
+val productTopicDetailsFlow = Flow[Product].map { x =>
+  println(s"\n\n${x}\n\n")
+  x
+}.map(r => Product(r.id, r.name, r.quantity))
+  .map(a => WriteMessage.createIndexMessage(a.id, a)).via(ElasticClient.esSink).map { messageResults =>
+  if (!messageResults.success) {
+    //      log.error(s"Error writing $docType objects to $esIndex: ${result.error.getOrElse("")}")
+    throw new Exception(s"Failed to write to ES")
+  }
+  Done
+}
+
 }
 
 class TopicSubscriber(productKafkaApi: ProductKafkaApi, productServiceFlow: ProductServiceFlow) {
-
-  productKafkaApi.productTopic.subscribe.atLeastOnce {
-    productServiceFlow.productTopicDetailsFlow
-  }
+  productKafkaApi.productTopic -> productServiceFlow.productTopicDetailsFlow
 }
